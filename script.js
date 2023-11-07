96a67863508c010cb77c266e384ceeb5994bc4d8
wilmaniklasson <wilma.niklasson2@hotmail.se>
2023-11-07 16:19:04 +0100

    mer frågor

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import { books } from './book-list.js'
 // Fråga 1.
 console.log ('Hur många böcker finns det i affären?')
-console.log(books.length)
+console.log('Det finns ' + books.length + ' böcker i affären.')
+console.log ( )
+
+
 /*2 Skriv ut namnen på alla böcker. (Skapa först en lista som bara innehåller namnen.)
 */
 
@@ -11,7 +14,7 @@ console.log(titles);
 
 
 /* 3 Skriv ut namn och pris för alla böcker av typen "Fantasy".*/
-
+console.log('Följande Fantasy-böcker finns tillgängliga i vår butik:')
 for (let i=0; i < books.length; i++){
 	if ( books[i].genre.includes("Fantasy"))
 	console.log(books[i].title, books[i].price)
