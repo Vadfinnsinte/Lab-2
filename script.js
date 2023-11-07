2c5fd5974a613423d82d9ad20f7f470a2e805365
wilmaniklasson <wilma.niklasson2@hotmail.se>
2023-11-07 17:07:24 +0100

    fråga 3

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ console.log ( )
 
 /*2 Skriv ut namnen på alla böcker. (Skapa först en lista som bara innehåller namnen.)
 */
+console.log('Detta samtliga titlar på böckerna:' )
 
 // Hämta titlarna från böckerna och lagra dem i en separat lista
 let titles = books.map(book => book.title);
 console.log(titles);
+console.log( )
 
 
 /* 3 Skriv ut namn och pris för alla böcker av typen "Fantasy".*/
@@ -19,4 +21,17 @@ for (let i=0; i < books.length; i++){
 	if ( books[i].genre.includes("Fantasy"))
 	console.log(books[i].title, books[i].price)
 } 
-  
+console.log( )
+
+
+/* 4 Skriv ut namn och genre för alla klassiker, dystopier och mysterieböcker.*/  
+
+console.log('Följande är namn och genre för alla: klassiker, dystopier och mysterieböcker:');
+for (let i = 0; i < books.length; i++) {
+    if (books[i].genre === "Classic" || books[i].genre === "Dystopian" || books[i].genre === "Mystery") {
+        console.log(books[i].title +'/', books[i].genre);
+    }
+}
+console.log( )
+
+
