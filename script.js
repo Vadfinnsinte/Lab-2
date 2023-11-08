2e905e23e663105b63e441c850a1850b8e0beafe
Linda Westerlund <lindawesterlund94@gmail.com>
2023-11-08 08:36:44 +0100

    svarat på fråga 5

--- script.js
+++ script.js
@@ -19,4 +19,9 @@ for (let i=0; i < books.length; i++){
 	if ( books[i].genre.includes("Fantasy"))
 	console.log(books[i].title, books[i].price)
 } 
-  
+// 5 Skriv ut namn och pris för alla böcker som kostar över $10.
+console.log("Dessa böcker kostar mer än $10");
+for (let i=0; i < books.length; i++){
+	if ( books[i].price > 10)
+	console.log(books[i].title, books[i].price)
+} 
