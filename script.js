b27bb4169304922c0bbfd4e5a9f4c73f67b903a5
Linda Westerlund <lindawesterlund94@gmail.com>
2023-11-08 09:50:13 +0100

    gjort fråga 6

--- script.js
+++ script.js
@@ -20,8 +20,21 @@ for (let i=0; i < books.length; i++){
 	console.log(books[i].title, books[i].price)
 } 
 // 5 Skriv ut namn och pris för alla böcker som kostar över $10.
-console.log("Dessa böcker kostar mer än $10");
+console.log("Dessa böcker kostar mer än $10: ");
 for (let i=0; i < books.length; i++){
 	if ( books[i].price > 10)
 	console.log(books[i].title, books[i].price)
 } 
+// 6 Hur mycket är hela bokinnehavet värt? (Vad är det totala priset, om man skulle sälja alla böcker?)
+
+let pris = books.map(book => book.price)
+let enpris = 0 
+// for (let i=0; i < pris.length; i++) {
+// 	enpris += pris[i]
+// 	console.log(enpris)
+// }
+pris.forEach(number => {
+	enpris += number
+})
+let prisavrund = enpris.toFixed(2)
+console.log("Totala summan för alla våra böcker(avrundat till två decimaler): ", Number(prisavrund));
